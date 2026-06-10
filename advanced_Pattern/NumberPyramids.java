6fb97d5d5f9bea4878ee510ade2b01223dc0f387
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-06-10 10:21:50 +0530

    number pyramid

---
+++ advanced_Pattern/NumberPyramids.java
@@ -0,0 +1,22 @@
+public class NumberPyramids{
+    public static void main(String[] args) {
+        int n=5;
+        // int counter=1;
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                    System.out.print(i);
+                    System.out.print(" ");           
+            }
+            
+            
+            System.out.println();
+            // counter++;
+        }
+    }
+
+    
+}
