edf1b4aceae064cb4ba15f2b4981b7dcba89ffaf
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-06-13 10:56:53 +0530

    hollow butterfly

---
+++ advanced_Pattern/HollowButterfly.java
@@ -0,0 +1,59 @@
+public class HollowButterfly {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+               
+                if(j==1 || i==1 || j==i){
+                    System.out.print("*");
+                }else{
+
+                    System.out.print(" ");
+                }
+            }
+
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+                // System.out.print("1");
+            }
+
+            for(int j=1;j<=i;j++){
+                 if( j==1 || i==j  ){
+                    System.out.print("*");
+                }else{
+
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+
+        for (int i = n; i >=1; i--) {
+            for (int j = 1; j <=i; j++) {
+                if(i==1||j==1||j==i){
+
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+                
+            }
+
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++){
+                if(j==i || j==1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
