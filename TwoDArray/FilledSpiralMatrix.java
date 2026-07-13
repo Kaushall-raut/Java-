d2ac421d08076dd5edf67f1d38fcdfe9f3bc5ead
Kaushall-raut <KaushalRaut.code@gmail.com>
2026-07-13 22:50:25 +0530

    matrix size

---
+++ TwoDArray/FilledSpiralMatrix.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FilledSpiralMatrix{
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of row and column");
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+
+        int matrix[][]=new int[row][col]; 
+
+        sc.close();
+
+
+
+    }
+}
